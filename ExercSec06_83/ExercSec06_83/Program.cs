4789dc084e230c96e261e87a0ba80e9f95607812
jgerlan <jgerlan@gmail.com>
2019-12-06 13:29:24 -0300

    [#0008] Implementa logica ExercSec06-83

--- ExercSec06_83/ExercSec06_83/Program.cs
+++ ExercSec06_83/ExercSec06_83/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ExercSec06_83
 {
@@ -6,7 +7,41 @@ namespace ExercSec06_83
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+            HashSet<int> total = new HashSet<int>();
+            int qtdCursos = 0;
+
+            Console.Write("O curso A possui quantos alunos? ");
+            qtdCursos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso A:");
+
+            for (int count = 0;count<qtdCursos;count++)
+            {
+                A.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.Write("O curso B possui quantos alunos? ");
+            qtdCursos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso B: ");
+            for (int count = 0; count < qtdCursos; count++)
+            {
+                B.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.Write("O curso C possui quantos alunos? ");
+            qtdCursos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite os códigos dos alunos do curso C: ");
+            for (int count = 0; count < qtdCursos; count++)
+            {
+                C.Add(int.Parse(Console.ReadLine()));
+            }
+            total.UnionWith(A);
+            total.UnionWith(B);
+            total.UnionWith(C);
+            
+            Console.Write("Total de alunos: {0} ", total.Count);
         }
     }
 }
