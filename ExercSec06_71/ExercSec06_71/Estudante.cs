9c71fe364b7dd446426c79d2510e7f1403e7084e
jgerlan <jgerlan@gmail.com>
2019-12-06 10:14:14 -0300

    [#0003] Adiciona Exercicios Secao 6 Cap 71

---
+++ ExercSec06_71/ExercSec06_71/Estudante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercSec06_71
+{
+    class Estudante
+    {
+        public string nomeEstudante { get; set; }
+        public string emailEstudante { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            nomeEstudante = nome;
+            emailEstudante = email;
+        }
+
+        public override string ToString()
+        {
+            return nomeEstudante + ", " + emailEstudante;
+        }
+    }
+}
---
+++ ExercSec06_71/ExercSec06_71/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExercSec06_71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] estudantes = new Estudante[10];
+            int numeroDeQuartos;
+
+            Console.Write("Quantos quartos vão ser alugados?");
+            numeroDeQuartos = int.Parse(Console.ReadLine());
+
+            
+            for (int loopCount = 0; loopCount < numeroDeQuartos; loopCount++)
+            {
+                Console.WriteLine($"Estudante # {loopCount+1}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email:");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+
+                estudantes[quarto] = new Estudante(nome, email);
+                //Console.WriteLine(loopCount + ": " + estudantes[quarto]);
+            }
+
+            for (int Count = 0; Count < 10; Count++)
+            {
+                if (estudantes[Count] != null) 
+                {
+                    Console.WriteLine(Count +": "+ estudantes[Count]);
+                }
+            }
+
+        }
+    }
+}
