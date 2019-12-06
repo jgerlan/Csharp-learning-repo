a8460d815e60b6ab3419aae906a2c7b399c8392c
jgerlan <jgerlan@gmail.com>
2019-12-06 10:12:01 -0300

    [#0002] Adiciona + Exercicios Secao 5

---
+++ ExercSec5/ExercSec5/Conta.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercSec5
+{
+    class Conta
+    {
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta() { }
+
+        public Conta(int numeroConta, string nomeTitular)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            //Saldo = 0;
+        }
+
+        public Conta(int numeroConta, string nomeTitular, double depositoInicial) : this (numeroConta, nomeTitular)
+        {
+            //NumeroConta = numeroConta;
+            //NomeTitular = nomeTitular;
+            DepositoSaldo(depositoInicial); ;
+        }
+
+        /*public int NumeroConta
+        {
+            get { return _numeroConta; }
+            set
+            {
+                if (value != 0 && _numeroConta == 0)
+                {
+                    _numeroConta = value;
+                }
+            }
+        }*/
+
+        public void DepositoSaldo(double valorDeposito)
+        {
+            Saldo = Saldo + valorDeposito;
+        }
+
+        public void SaqueSaldo(double valorSaque)
+        {
+            if (valorSaque > 0)
+            {
+                Saldo = Saldo - 5.00 - valorSaque;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroConta
+                + ", Titular: "
+                + NomeTitular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExercSec5/ExercSec5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercSec5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+            int numeroConta = 0;
+            string titular = string.Empty;
+            double deposito = 0.0;
+            double saque = 0.0;
+            char haDepositoInicial;
+            
+
+            //while (true)
+            //{
+                conta = new Conta();
+
+                Console.Write("Entre com o número da conta: ");
+                numeroConta = int.Parse(Console.ReadLine());
+                Console.Write("Entre com o titular da conta: ");
+                titular = Console.ReadLine();
+                Console.Write("Haverá depósito inicial (s/n): ");
+                haDepositoInicial = char.Parse(Console.ReadLine());
+
+                if (haDepositoInicial == 's' || haDepositoInicial == 'S')
+                {
+                    Console.Write("Entre com o valor inicial: ");
+                    deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    conta = new Conta(numeroConta, titular, deposito);
+                }else
+                {
+                    conta = new Conta(numeroConta, titular);
+                }
+
+                //Console.WriteLine("\nDados da conta:\nConta:" + conta.NumeroConta + " Titular: " + conta.NomeTitular + " Saldo: " + conta.Saldo);
+                Console.WriteLine("\nDados da conta: ");
+                Console.WriteLine(conta);
+
+
+                Console.Write("Entre com um valor para deposito:");
+                conta.DepositoSaldo(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+                Console.Write("Entre um valor para Saque (Taxa $5.00):");
+                conta.SaqueSaldo(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+                //Console.WriteLine("\nDados da conta atualizados:\nConta:" + conta.NumeroConta + " Titular: " + conta.NomeTitular + " Saldo: " + conta.Saldo);
+                Console.WriteLine("\nDados da conta atualizados: ");
+                Console.WriteLine(conta);
+            //}
+
+
+        }
+    }
+}
