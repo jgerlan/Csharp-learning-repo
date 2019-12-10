9cae4759b457e9e009b9b8b3b8437036a1145499
Gerlan de Paula <jgerlan@gmail.com>
2019-12-10 17:42:01 -0300

    [#0016] Inicia implementacao classes

---
+++ ExercSec06_132/ExercSec06_132/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercSec06_132.Entities
+{
+    class Product
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public string pricetag()
+        {
+
+            return "";
+        }
+    }
+}
---
+++ ExercSec06_132/ExercSec06_132/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercSec06_132
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
