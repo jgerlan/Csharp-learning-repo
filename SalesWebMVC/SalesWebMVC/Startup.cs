1c5ff6753ca59bb86c8d38f8d66fbf0477c7e4db
Gerlan de Paula <jgerlan@gmail.com>
2019-12-17 16:53:01 -0300

    [#0028] Modifica tipo de sgbd e cria migrations para o banco

--- SalesWebMVC/SalesWebMVC/Startup.cs
+++ SalesWebMVC/SalesWebMVC/Startup.cs
@@ -27,8 +27,10 @@ namespace SalesWebMVC
         {
             services.AddControllersWithViews();
 
+            //options.UseSqlServer(Configuration.GetConnectionString("SalesWebMVCContext"))
             services.AddDbContext<SalesWebMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMVCContext"), builder => 
+                        builder.MigrationsAssembly("SalesWebMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
